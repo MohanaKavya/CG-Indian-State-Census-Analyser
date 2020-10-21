f264ae714708e5fd7beaff443c89d4d7698f2a4d
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-21 16:12:01 +0530

    Added : Welcome Message is Added

---
+++ main/java/com/capgemini/indiancensusanalyser/StateCensusAnalyser.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.capgemini.indiancensusanalyser;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.nio.file.Paths;
+import java.util.logging.Logger;
+
+/**
+ * @author Mohana Kavya
+ *
+ */
+public class StateCensusAnalyser {
+	public static void main(String[] args) {
+		Logger log = Logger.getLogger(StateCensusAnalyser.class.getName());
+		log.info("Welcome to Indian State Census Analyser");
+	}
+}
