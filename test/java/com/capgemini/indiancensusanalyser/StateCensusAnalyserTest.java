8c1d3c768b58e19bde5b1a7037c493dcc1e07538
Mohana Kavya K <kkunisettymohana@student.nitw.ac.in>
2020-10-21 23:55:06 +0530

    Added : New Test Case to Check Delimiter in .csv file is Added

--- test/java/com/capgemini/indiancensusanalyser/StateCensusAnalyserTest.java
+++ test/java/com/capgemini/indiancensusanalyser/StateCensusAnalyserTest.java
@@ -19,6 +19,7 @@ public class StateCensusAnalyserTest {
     private static final String INDIA_CENSUS_CSV_FILE_PATH = "./src/test/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
     private static final String WRONG_CSV_FILE_TYPE="./src/test/resources/IndiaStateCensusData.java";
+	private static final String WRONG_CSV_DELIMITER = "./src/test/resources/IndiaStateCensusDataWrongDelimiter.csv";
 
     // Happy Test Case
     @Test
@@ -39,7 +40,7 @@ public class StateCensusAnalyserTest {
 			censusAnalyser.loadStateCensus(WRONG_CSV_FILE_PATH);
 		} catch (CensusAnalyserException e) {
 			Assert.assertEquals(ExceptionType.CENSUS_FILE_PROBLEM, e.type);
-        	log.severe("Exception occured : "+e.getMessage());
+        	log.severe("Exception occured - Wrong File Path: "+e.getMessage());
 		}
 	}
     // Sad Test Case
@@ -50,7 +51,18 @@ public class StateCensusAnalyserTest {
 			censusAnalyser.loadStateCensus(WRONG_CSV_FILE_TYPE);
 		} catch (CensusAnalyserException e) {
 			Assert.assertEquals(ExceptionType.CENSUS_FILE_PROBLEM, e.type);
-        	log.severe("Exception occured : "+e.getMessage());
+        	log.severe("Exception occured - Wrong File Type: "+e.getMessage());
+		}
+	}
+    // Sad Test Case
+    @Test
+	public void givenWrongDelimiter_InIndiaCensusData_ShouldReturnCustomExceptionType() {
+		StateCensusAnalyser censusAnalyser = new StateCensusAnalyser();
+		try {
+			censusAnalyser.loadStateCensus(WRONG_CSV_DELIMITER);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        	log.severe("Exception occured - Wrong Delimiter: "+e.getMessage());
 		}
 	}
 }
